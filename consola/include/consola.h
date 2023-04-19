489d3b27faf12658ab1d2da8451af2e6bae3c34d
joselincondori <jocondori@frba.utn.edu.ar>
2023-04-19 04:11:46 +0000

    Correccion checkpoint 1

--- consola/include/consola.h
+++ consola/include/consola.h
@@ -1,22 +1,25 @@
 #ifndef CONSOLA_H
 #define CONSOLA_H
+
 #include <stdio.h>
-#include <commons/log.h>
-#include <commons/config.h>
-#include <commons/string.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-//includes de libs
-//#include "server.h"
-#include "client.h"
 #include "utils.h"
+#include "client_handle.h"
 
 // includes del modulo
 #include "parser.h"
 
-t_config* initialize_config_console(char* config_path);
-void destroy_console(t_config* config_console, t_log* logger_console, int kernel_socket);
+
+void start_console_client(char* ip_kernel, char* puerto_kernel, t_log* logger_console);
 
 
+t_log* logger_console; 
+t_log* logger_aux;
+
+char* config_console_path = "/home/utnso/Documents/tp-2023-1c-KernelFlow/consola/config/console.config";
+char* logger_console_path = "/home/utnso/Documents/tp-2023-1c-KernelFlow/consola/consola.log";
+char* logger_aux_path = "/home/utnso/Documents/tp-2023-1c-KernelFlow/consola/consola_aux.log";
 
 #endif
--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,74 +1,37 @@
 #include "consola.h"
 
+int main(int argc, char** argv){
 
+  // inicializo loggers
+  logger_aux = log_create(logger_aux_path, "Consola_aux", 1, LOG_LEVEL_DEBUG);
+  logger_console = log_create(logger_console_path, "Consola", 1, LOG_LEVEL_DEBUG);
 
-int main(int argc, char** argv){ 
+  // leer configuracion
+  t_config_console* config = NULL;
+  config = read_config(config_console_path, logger_console);
 
-   char* ip_kernel;
-   char* puerto_kernel;
+  if(config == NULL){
+      end_program(logger_console, logger_aux, config);
+      return EXIT_FAILURE;
+    }
 
-   t_log* logger_console;
-   t_config* config_console;
-
-   int kernel_socket;
-
-   FILE* archivo_instrucciones = fopen(argv[1], "r");
-
-   logger_console = log_create("consola.log", "Consola", true, LOG_LEVEL_DEBUG);
-
-   if(logger_console == NULL){
+  if(logger_console == NULL){
       log_error(logger_console, "No se pudo iniciar la consola"),
-      destroy_console(config_console, logger_console, kernel_socket);
+      end_program(logger_console, logger_aux, config);
+      return EXIT_FAILURE;
    }
 
-   log_info(logger_console, "Consola iniciada.");
+  
+  log_debug(logger_console, "Consola iniciada.");
 
-   config_console = initialize_config_console("/home/utnso/Documents/tp-2023-1c-KernelFlow/consola/bin/console.config");
+  //inicializo socket
+  start_console_client(config->ip_kernel, config->puerto_kernel, logger_console);
 
-   ip_kernel = config_get_string_value(config_console, "IP_KERNEL");
-   puerto_kernel = config_get_string_value(config_console, "PUERTO_KERNEL");
+  // fin del programa
+  end_program(logger_console, logger_aux, config);
+  return EXIT_SUCCESS;
+}
 
-   kernel_socket = create_connection(ip_kernel, puerto_kernel); 
 
-   /*-------PARSEAR PSEUDOCODIGO--------
 
-   if(!el_proceso_es_valido(archivo_instrucciones)){
-      log_error(logger_console, "El proceso no es valido");
-      destroy_console(config_console, logger_console, kernel_socket);
-   }
 
-   ------ENVIAR LISTA INSTRUCCIONES A KERNEL--------
-
-   send_package(archivo_instrucciones, kernel_socket);
-
-   wait_kernel(kernel_socket);*/
-
-   destroy_console(config_console, logger_console, kernel_socket);
-   
-   return 0;
-};
-
-t_config* initialize_config_console(char* config_path){
-
-    t_config* config_console = config_create(config_path);
-    
-    if(config_console == NULL){
-      printf("NO se pudo configurar el archivo de configuraciones");
-      exit(1);
-    }
-
-    else{
-      return config_console; }
-};
-
-void destroy_console(t_config* config_console, t_log* logger_console, int kernel_socket){
-    if(logger_console != NULL){
-      log_destroy(logger_console);
-    }
-    if(config_console != NULL){
-      config_destroy(config_console);
-    }
-    close(kernel_socket);
-   }
-
-wait_kernel(int kernel_socket){}
