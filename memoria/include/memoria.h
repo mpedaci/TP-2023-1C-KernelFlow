0ad1ea7ae34e44de27945ee5dede8193405b3a02
Marcos Pedaci <mpedaci@frba.utn.edu.ar>
2023-05-31 20:44:05 -0300

    Fix memory

--- memoria/include/memoria.h
+++ memoria/include/memoria.h
@@ -5,8 +5,8 @@
 #include "estructuras.h"
 #include "memory_server.h"
 
-char *config_path = "/home/utnso/Documents/Trabajos/tp-2023-1c-KernelFlow/memoria/conf/memoria.config";
-char *logger_main_path = "/home/utnso/Documents/Trabajos/tp-2023-1c-KernelFlow/memoria/logs/memoria.log";
-char *logger_aux_path = "/home/utnso/Documents/Trabajos/tp-2023-1c-KernelFlow/memoria/logs/memoria_aux.log";
+char *config_path = "./conf/memoria.config";
+char *logger_main_path = "./logs/memoria.log";
+char *logger_aux_path = "./logs/memoria_aux.log";
 
 #endif /* MEMORIA_H */
