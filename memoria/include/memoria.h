8bbfa0beb56ebd9cc207dc969e25dce6d965feba
Marcos Pedaci <mpedaci@frba.utn.edu.ar>
2023-06-06 20:14:06 -0300

    Fix memory config name

--- memoria/include/memoria.h
+++ memoria/include/memoria.h
@@ -5,7 +5,7 @@
 #include "estructuras.h"
 #include "memory_server.h"
 
-char *config_path = "./conf/memoria.config";
+char *config_path = "./config/memoria.config";
 char *logger_main_path = "./logs/memoria.log";
 char *logger_aux_path = "./logs/memoria_aux.log";
 
