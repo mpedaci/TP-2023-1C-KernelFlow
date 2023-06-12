ae8042ba704144f41a31f134558fdb52298fd83d
Juan Martin Vazquez <juavazquez@frba.utn.edu.ar>
2023-06-12 21:02:57 +0000

    ahora si mergeado con cpu

--- cpu/src/mmu.c
+++ cpu/src/mmu.c
@@ -3,7 +3,6 @@
 // este mmu es una cagada, para mi no esta bien
 // esto es para paginacion, no para segmentacion
 
-<<<<<<< HEAD
 int get_num_segmento(char *dir_logica)
 {
     // int tam_max_segmento = atoi(config->tam_max_segmento);
@@ -30,21 +29,6 @@ int get_desplazamiento_segmento(char *dir_logica)
 }
 
 char *get_direccion_fisica(char *dir_logica, t_list *segments)
-=======
-int get_num_segmento(int dir_logica)
-{
-    int tam_max_segmento = atoi(config->tam_max_segmento);
-    return (int)floor(dir_logica / tam_max_segmento);
-}
-
-int get_desplazamiento_segmento(int dir_logica)
-{
-    int tam_max_segmento = atoi(config->tam_max_segmento);
-    return dir_logica % tam_max_segmento;
-}
-
-int get_direccion_fisica(int dir_logica, t_list *segmentos)
->>>>>>> cpu
 {
     // agarrar el segmento que corresponde con el numero de segmento
     // agarrar el desplazamiento que corresponde con la direccion logica
@@ -52,7 +36,6 @@ int get_direccion_fisica(int dir_logica, t_list *segmentos)
     // devolver la direccion fisica
     int num_segmento = get_num_segmento(dir_logica);
     int desplazamiento = get_desplazamiento_segmento(dir_logica);
-<<<<<<< HEAD
     t_segment *segmento = get_by_num_segmento(num_segmento, segments);
     int inicio_segmento = segmento->base_address;
     return string_itoa(inicio_segmento + desplazamiento);
@@ -65,20 +48,6 @@ t_segment *get_by_num_segmento(int num_segment, t_list *segments)
     {
         segmento = list_get(segments, i);
         if (segmento->id == num_segment)
-=======
-    t_segment *segmento = get_by_num_segmento(num_segmento, segmentos);
-    int inicio_segmento = segmento->base_address;
-    return inicio_segmento + desplazamiento;
-}
-
-t_segment *get_by_num_segmento(int num_segmento, t_list *segmentos)
-{
-    t_segment *segmento;
-    for (int i = 0; i < list_size(segmentos); i++)
-    {
-        segmento = list_get(segmentos, i);
-        if (segmento->id == num_segmento)
->>>>>>> cpu
             return segmento;
     }
     return NULL;
