bf0d634b949d15dc2411e31f5f11940cfc4840b2
Juan Martin Vazquez <juavazquez@frba.utn.edu.ar>
2023-06-14 23:08:09 +0000

    arreglado mmu

--- cpu/src/mmu.c
+++ cpu/src/mmu.c
@@ -1,39 +1,19 @@
 #include "mmu.h"
 
-// este mmu es una cagada, para mi no esta bien
-// esto es para paginacion, no para segmentacion
-
 int get_num_segmento(char *dir_logica)
 {
-    // int tam_max_segmento = atoi(config->tam_max_segmento);
-    // return (int)floor(atoi(dir_logica) / tam_max_segmento);
-
-    /* 1234
-        esto deberia ser asi
-
-        seteamos como default que los dos primeros n numeros de la direccion logica son el numero de segmento
-
-        agarramos los primeros n numeros de la direccion logica y los convertimos a int
-
-        y ya tenemos el nuemro de segmento, luego los otros numeros son el desplazamiento
-    */
-
-    return 1;
+    int tam_max_segmento = atoi(config->tam_max_segmento);
+    return (int)floor(atoi(dir_logica) / tam_max_segmento);
 }
 
 int get_desplazamiento_segmento(char *dir_logica)
 {
-    // int tam_max_segmento = atoi(config->tam_max_segmento);
-    // return atoi(dir_logica) % tam_max_segmento;
-    return 0;
+    int tam_max_segmento = atoi(config->tam_max_segmento);
+    return atoi(dir_logica) % tam_max_segmento;
 }
 
 char *get_direccion_fisica(char *dir_logica, t_list *segments)
 {
-    // agarrar el segmento que corresponde con el numero de segmento
-    // agarrar el desplazamiento que corresponde con la direccion logica
-    // sumar el desplazamiento al inicio del segmento
-    // devolver la direccion fisica
     int num_segmento = get_num_segmento(dir_logica);
     int desplazamiento = get_desplazamiento_segmento(dir_logica);
     t_segment *segmento = get_by_num_segmento(num_segmento, segments);
