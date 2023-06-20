8b80aadbd3aec2e74e57328f138053d055a2d045
Marcos Pedaci <mpedaci@frba.utn.edu.ar>
2023-06-20 19:43:07 -0300

    Add Memoria -> CPU

--- memoria/include/modulos.h
+++ memoria/include/modulos.h
@@ -5,9 +5,10 @@
 #include "types_controllers.h"
 
 void kernel_operations(int client_socket);
-void handle_pid_instruction(int client_socket, t_pid_instruccion *pidtruction);
+void handle_kernel_pid_instruction(int client_socket, t_pid_instruccion *pidtruction);
 void handle_pid_status(int client_socket, t_pid_status *pid_status);
 void cpu_operations(int client_socket);
+void handle_cpu_pid_instruction(int client_socket, t_pid_instruccion *pidtruction);
 void fs_operations(int client_socket);
 //void cpu_and_fs_operations(int client_socket,char *module);
 
--- memoria/src/modulos.c
+++ memoria/src/modulos.c
@@ -17,7 +17,7 @@ void kernel_operations(int client_socket)
             break;
         case PID_INSTRUCCION:
             t_pid_instruccion *pidtruction = get_pid_instruccion(package);
-            handle_pid_instruction(client_socket, pidtruction);
+            handle_kernel_pid_instruction(client_socket, pidtruction);
             instruccion_destroy(pidtruction->instruccion);
             free(pidtruction);
             break;
@@ -41,7 +41,7 @@ void kernel_operations(int client_socket)
     }
 }
 
-void handle_pid_instruction(int client_socket, t_pid_instruccion *pidtruction)
+void handle_kernel_pid_instruction(int client_socket, t_pid_instruccion *pidtruction)
 {
     t_instruccion *instruccion = pidtruction->instruccion;
     int pid = pidtruction->pid;
@@ -78,11 +78,6 @@ void handle_pid_instruction(int client_socket, t_pid_instruccion *pidtruction)
         t_segments_table *segments_table = get_segments_table_by_pid(pid);
         send_tsegmento(client_socket, segments_table, logger_aux);
         break;
-    case I_EXIT:
-        log_info(logger_main, "PID: %d - Finalizar Proceso", pid);
-        delete_segments_table(get_segments_table_by_pid(pid));
-        send_status_code(client_socket, SUCCESS, logger_aux);
-        break;
     default:
         log_warning(logger_aux, "Instruccion desconocida");
         break;
@@ -96,9 +91,11 @@ void handle_pid_status(int client_socket, t_pid_status *pid_status)
     {
     case PROCESS_NEW:
         t_segments_table *st = create_segments_table(pid_status->pid);
+        log_info(logger_main, "Creacion de Proceso PID: %d", pid_status->pid);
         send_tsegmento(client_socket, st, logger_aux);
         break;
     case PROCESS_END:
+        log_info(logger_main, "Eliminacion de Proceso PID: %d", pid_status->pid);
         delete_segments_table(get_segments_table_by_pid(pid_status->pid));
         break;
     default:
@@ -115,52 +112,61 @@ void cpu_operations(int client_socket)
         t_package *package = get_package(client_socket, logger_aux);
         switch (package->operation_code)
         {
-        case INSTRUCCION:
-            t_instruccion *instruction = get_instruccion(package);
-            bool res;
-            int base_address;
-            int length;
-            switch (instruction->identificador)
-            {
-            case I_MOV_IN: // lee de memoria y pasa valor leido
-                base_address = atoi((char *)list_get(instruction->parametros, 0));
-                length = atoi((char *)list_get(instruction->parametros, 1));
-
-                // t_info *info = read_memory(base_address, length);
-                // res = send_info(client_socket, info, logger_aux);
-                // if(!res)
-                //     log_error(logger_aux, "No se pudo enviar el valor leido de memoria a CPU (MOV_IN)");
-                // HACER UN FREE DE INFO POR FAVOR
-                break;
-            case I_MOV_OUT: // escribe en memoria y pasa OK
-                base_address = atoi((char *)list_get(instruction->parametros, 0));
-                char *valor_a_escribir = (char *)list_get(instruction->parametros, 1);
-                length = instruction->p_length[1];
-
-                bool result = write_memory(base_address, length, valor_a_escribir);
-                if (!result)
-                    log_error(logger_aux, "No se pudo escribir en memoria (MOV_OUT)");
-
-                res = send_status_code(client_socket, SUCCESS, logger_aux);
-                if (!res)
-                    log_error(logger_aux, "No se pudo enviar el OK a CPU (MOV_OUT)");
-                break;
-            default:
-                log_warning(logger_aux, "Instruccion desconocida");
-                exit = true;
-                break;
-            }
-            // HACER UN FREE DE INSTRUCCION POR FAVOR
+        case PID_INSTRUCCION:
+            t_pid_instruccion *pidtruction = get_pid_instruccion(package);
+            handle_cpu_pid_instruction(client_socket, pidtruction);
+            instruccion_destroy(pidtruction->instruccion);
+            free(pidtruction);
         case END:
-            log_warning(logger_aux, "CPU: Conexion Finalizada");
+            log_info(logger_aux, "Conexion Finalizada");
             exit = true;
             break;
         default:
-            log_warning(logger_aux, "CPU: Operacion desconocida\n");
+            log_warning(logger_aux, "Operacion desconocida\n");
             exit = true;
             break;
         }
         package_destroy(package);
+    }
+}
+
+void handle_cpu_pid_instruction(int client_socket, t_pid_instruccion *pidtruction)
+{
+    t_instruccion *instruction = pidtruction->instruccion;
+    bool res = false;
+    int base_address = 0;
+    int length = 0;
+    switch (instruction->identificador)
+    {
+    case I_MOV_IN: // lee de memoria y pasa valor leido
+        base_address = atoi((char *)list_get(instruction->parametros, 0));
+        length = atoi((char *)list_get(instruction->parametros, 1));
+        // t_info *info = read_memory(base_address, length);
+        // log_info(logger_main, "PID: %d - Acción: LEER - Dirección física: %d - Tamaño: %d - Origen: CPU ", pidtruction->pid, base_address, length);
+        // res = send_info(client_socket, info, logger_aux);
+        // if(!res)
+        //     log_error(logger_aux, "No se pudo enviar el valor leido de memoria a CPU (MOV_IN)");
+        break;
+    case I_MOV_OUT: // escribe en memoria y pasa OK
+        base_address = atoi((char *)list_get(instruction->parametros, 0));
+        char *valor_a_escribir = (char *)list_get(instruction->parametros, 1);
+        length = instruction->p_length[1];
+        bool result = write_memory(base_address, length, valor_a_escribir);
+        if (!result)
+        {
+            log_error(logger_aux, "No se pudo escribir en memoria (MOV_OUT)");
+            res = send_status_code(client_socket, SEGMENTATION_FAULT, logger_aux);
+        }
+        else
+        {
+            log_info(logger_main, "PID: %d - Acción: ESCRIBIR - Dirección física: %d - Tamaño: %d - Origen: CPU ", pidtruction->pid, base_address, length);
+            res = send_status_code(client_socket, SUCCESS, logger_aux);
+        }
+        if (!res)
+            log_error(logger_aux, "No se pudo enviar el OK a CPU (MOV_OUT)");
+        break;
+    default:
+        log_warning(logger_aux, "Instruccion desconocida");
     }
 }
 
